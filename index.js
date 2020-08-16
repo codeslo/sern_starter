c33eb2d43cd564894bf495763e7454ca3b9bc223
matt-codeslo <matt@codeslo.com>
2020-08-16 14:25:52 -0700

    adding no cache

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const express = require("express");
 const port = process.env.PORT;
 const path = require("path");
 const app = express();
+const nocache = require("nocache");
 // routes
 const messageRoutes = require("./routes/messageRoutes");
 
 // end routes
-
+app.use("nocache()");
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use("/message", messageRoutes);
 
