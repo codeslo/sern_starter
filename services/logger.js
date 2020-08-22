0dcce2ae18ef128288ec3e56b375c303ff83a8d1
matt-codeslo <matt@codeslo.com>
2020-08-22 15:04:21 -0700

    adding logging configuration

--- services/logger.js
+++ services/logger.js
@@ -1,12 +1,35 @@
 const winston = require("winston");
 
+// module.exports = logger = winston.createLogger({
+//   level: "info",
+//   format: winston.format.combine(
+//     winston.format.timestamp(),
+//     winston.format.printf((info) => {
+//       return `${info.timestamp} ${info.level}: ${info.message}`;
+//     })
+//   ),
+//   transports: [new winston.transports.Console()],
+// });
+
 module.exports = logger = winston.createLogger({
   level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf((info) => {
-      return `${info.timestamp} ${info.level}: ${info.message}`;
-    })
-  ),
-  transports: [new winston.transports.Console()],
+  format: winston.format.json(),
+  defaultMeta: {
+    service: "user-service",
+  },
+  transports: [
+    new winston.transports.File({
+      filename: "logs/all.log",
+      level: "all",
+    }),
+  ],
 });
+
+// if we're local, log to console
+if (process.env.NODE_ENV == "local") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
